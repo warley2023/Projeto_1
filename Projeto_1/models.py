0aec773bb5cbcb37358a8de822f5a6dad3f2445b
JOSÉ GEOVANE DE OLIVEIRA ESTEVÃO <20231198060011@ifrn.local>
2025-02-12 15:43:34 -0300

    Primeiro Commit

---
+++ Projeto_1/models.py
@@ -0,0 +1,15 @@
+from django.db import models;
+
+class Paciente(models.Model):
+    Nome = models.CharField(max_length=100)
+    CPF = models.CharField(unique=True)
+    telefone = models.CharField(max_length=15, blank=True, null=True)
+    Data_Nascimento = models.DateTimeField(auto_now_add=True)
+    Endereco = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"{self.Nome} - {self.telefone} - {self.CPF} - {self.Data_Nascimento} - {self.Endereco}"
+
+class Cores(models.Model):
+    Nome = models.CharField(max_length=15)
+    
